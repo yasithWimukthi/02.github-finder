e2124ee038b01cd8335fb520882ce89e1dd6d63a
yasithWimukthi <it19966922@my.sliit.lk>
2020-10-31 16:57:29 +0530

    create Users.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Navbar from './components/layout/Navbar';
-import UserItem from './components/users/UserItem'
+import Users from './components/users/Users'
 import './App.css';
 
 class App extends React.Component {
@@ -9,7 +9,11 @@ class App extends React.Component {
     return (
       <div className="App">
           <Navbar />
-          <UserItem />
+
+          <div className="container">
+              <Users />
+          </div>
+          
       </div>
     );
   }
---
+++ src/components/users/Users.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import UserItem from './UserItem';
+
+export class Users extends Component {
+
+    state ={
+        users : [
+            {
+                id : '1',
+                login : 'mojombo',
+                avatar_url : 'https://avatars0.githubusercontent.com/u/1?v=4',
+                html_url : 'https://github.com/mojombo'
+            },
+            {
+                id : '2',
+                login : 'defunkt',
+                avatar_url : 'https://avatars0.githubusercontent.com/u/2?v=4',
+                html_url : 'https://github.com/mojombo'
+            }
+        ]
+    }
+
+    render() {
+        return (
+            <div style={userStyle}>
+                {
+                    this.state.users.map(user =>(
+                        <UserItem user={user} key={user.id}/>
+                    ))
+                }
+            </div>
+        )
+    }
+}
+
+const userStyle = {
+    display : 'grid',
+    gridTemplateColumns : 'repeat(3,1fr)',
+    gridGap : '1rem'
+}
+
+export default Users;
