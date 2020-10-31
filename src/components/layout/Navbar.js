888ce3ade7189432980be1aad8d74889718219c0
yasithWimukthi <it19966922@my.sliit.lk>
2020-10-31 15:36:58 +0530

    edit Navbar.js

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,10 +1,22 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export class Navbar extends Component {
+
+    static defaultProps = {
+        title : 'Github Finder',
+        icon : 'fab fa-github'
+    }
+
+    static propTypes = {
+        title : PropTypes.string.isRequired,
+        icon : PropTypes.string.isRequired
+    };
+
     render() {
         return (
             <div>
-                <h1>navbar</h1>
+                <i className={this.props.icon}></i> {this.props.title}
             </div>
         )
     }
