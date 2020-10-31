ece5b6895ec13a0c376b7a58f83656ce869d5aa7
yasithWimukthi <it19966922@my.sliit.lk>
2020-10-31 17:55:20 +0530

    convert class components to functional components

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,24 +1,30 @@
 
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-export class UserItem extends Component {
+const UserItem =  props => {
 
-    render() {
+    const {login,avatar_url,html_url} = props.user;
 
-        const {login,avatar_url,html_url} = this.props.user;
+    return (
+        <div className="card text-center">
+            <img src={avatar_url} 
+            alt ="" 
+            className="round-img" 
+            style={{width : "60px"}}/>
 
-        return (
-            <div className="card text-center">
-                <img src={avatar_url} alt ="" className="round-img" style={{width : "60px"}}/>
+            <h3>{login}</h3>
 
-                <h3>{login}</h3>
-
-                <div>
-                    <a href={html_url} className=" btn btn-dark btn-sm my-1"> More</a>
-                </div>
+            <div>
+                <a href={html_url} className=" btn btn-dark btn-sm my-1"> More</a>
             </div>
-        )
-    }
+        </div>
+    )
+    
+}
+
+UserItem.prototype = {
+    user : PropTypes.object.isRequired
 }
 
 export default UserItem
