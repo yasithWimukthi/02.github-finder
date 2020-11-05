9a7f1840149bf01240906a44c564dae3e6a3d606
yasithWimukthi <it19966922@my.sliit.lk>
2020-11-05 14:32:08 +0530

    edit About.js

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -5,7 +5,7 @@ export const About = () => {
         <Fragment>
             <h1>About this app.</h1>
             <p>App to search github users.</p>
-            <p>Version 1.0.0</p>
+            <p>Version 1.0</p>
         </Fragment>
     );
 }
