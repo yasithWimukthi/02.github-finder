c0a66d86e4b112e65777d7f3a6a2a157e1b776e0
yasithWimukthi <it19966922@my.sliit.lk>
2020-11-06 15:38:41 +0530

    modify User.js

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,9 +1,18 @@
-import React, { Component } from 'react'
+import React, { Component,Fragment } from 'react';
+import Spinner from '../layout/Spinner';
+import PropTypes from 'prop-types';
+import {Link} from 'react-router-dom'
 
 export class User extends Component {
 
     componentDidMount(){
         this.props.getUser(this.props.match.params.login);
+    }
+
+    static propTypes = {
+        loading : PropTypes.bool,
+        user : PropTypes.object.isRequired,
+        getUser : PropTypes.func.isRequired
     }
 
     render() {
@@ -23,8 +32,16 @@ export class User extends Component {
         } = this.props.user;
 
         const {loading} = this.props;
+
+        if (loading){
+            return <Spinner />;
+        }
+
         return (
-            <div>{name} </div>
+            <Fragment>
+                <Link to='/' className='btn btn-light'>Back To Search</Link>
+            </Fragment>
+            
         )
     }
 }
